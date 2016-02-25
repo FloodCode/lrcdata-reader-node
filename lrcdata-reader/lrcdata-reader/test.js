9629abcb20d97cf4eb50bb3f312d4959368700c5
Sergiy Varakuta <flood.code@gmail.com>
2016-02-25 20:53:47 +0200

    First release

---
+++ lrcdata-reader/lrcdata-reader/test.js
@@ -0,0 +1,15 @@
+﻿var fs = require('fs');
+var lrcdataReader = require('./lrcdata-reader.js');
+
+fs.open('lrcdata.bin', 'r', function (status, fd) {
+    if (status) {
+        console.log(status.message);
+        return;
+    }
+    
+    var stats = fs.statSync('lrcdata.bin');
+    var buffer = new Buffer(stats['size']);
+    fs.read(fd, buffer, 0, stats['size'], 0);
+    var lrcdata = lrcdataReader.read(buffer);
+    console.log(lrcdata);    
+});
