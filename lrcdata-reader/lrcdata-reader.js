462eb605cf0a35abf86d9fb8fc0e53027f90d582
Sergiy Varakuta <flood.code@gmail.com>
2016-03-08 18:47:48 +0200

    Updated data parser

--- lrcdata-reader/lrcdata-reader.js
+++ lrcdata-reader/lrcdata-reader.js
@@ -18,15 +18,15 @@
         switch (header.type) {
             case 0x0:
                 // Error
-                result.data = readError(rawData);
+                result.data = readErrorData(rawData);
                 break;
             case 0x1:
                 // Keyboard
-                result.data = readKeyboard(rawData);
+                result.data = readKeyboardData(rawData);
                 break;
             case 0x2:
                 // Clipboard
-                result.data = readClipboard(rawData);
+                result.data = readClipboardData(rawData);
                 break;
             default:
                 // Unknown type
@@ -66,49 +66,28 @@ function readHeader(buffer) {
     return result;
 }
 
-function readError(buffer) {
+function readErrorData(buffer) {
     var code = buffer.readUInt32BE(0);
     var message = readString(buffer, 4);
     return { code: code, message: message };
 }
 
-function readKeyboard(buffer) {
+function readKeyboardData(buffer) {
     var offset = 0x0;
     var count = buffer.readUInt32BE(offset);
     offset += 4;
     
     var items = [];
     for (var i = 0; i < count; i++) {
-        var kbdInfo = {};
-        
-        kbdInfo.subtype = buffer.readInt8(offset);
-        offset += 1;
-        
-        switch (kbdInfo.subtype) {
-            case 0x1:
-                // VKInfo
-                var vkInfo = readVKInfo(buffer, offset);
-                offset = vkInfo.offset;
-                kbdInfo.vkInfo = vkInfo.data;
-                break;
-            case 0x2:
-                // WNDInfo
-                var wndInfo = readWNDInfo(buffer, offset);
-                offset = wndInfo.offset;
-                kbdInfo.wndInfo = wndInfo.data;
-                break;
-            default:
-                // Unknown subtype
-                return null;
-        }
-        
-        items.push(kbdInfo);
+        var kbd = readKeyboard(buffer, offset);
+        offset = kbd.offset;
+        items.push(kbd.data);
     }
     
     return { count: count, items: items };
 }
 
-function readClipboard(buffer) {
+function readClipboardData(buffer) {
     var offset = 0x0;
     var count = buffer.readUInt32BE(offset);
     offset += 4;
@@ -119,10 +98,31 @@ function readClipboard(buffer) {
         offset = wndInfo.offset;
         var data = readString(buffer, offset);
         offset = data.offset;
-        items.push({ time: time, wndInfo: wndInfo.data, data: data.data });
+        items.push({ wndInfo: wndInfo.data, data: data.data });
     }
     
     return { count: count, items: items };
+}
+
+function readKeyboard(buffer, offset) {
+    var wndInfo = readWNDInfo(buffer, offset);
+    var count = buffer.readUInt32BE(wndInfo.offset);
+    offset = wndInfo.offset + 4;
+
+    var keyboard = {};
+    keyboard.data = {};
+    keyboard.data.wndInfo = wndInfo.data;
+    keyboard.data.count = count;
+    keyboard.data.keys = [];
+
+    for (var i = 0; i < count; i++) {
+        var vkInfo = readVKInfo(buffer, offset);
+        offset = vkInfo.offset;
+        keyboard.data.keys.push(vkInfo);
+    }
+
+    keyboard.offset = offset;
+    return keyboard;
 }
 
 function readVKInfo(buffer, offset) {
@@ -136,12 +136,13 @@ function readVKInfo(buffer, offset) {
 }
 
 function readWNDInfo(buffer, offset) {
+    var time = buffer.readUInt32BE(offset);
     var process = readString(buffer, offset + 4);
     var title = readString(buffer, process.offset);
     
     var wndInfo = {};
     wndInfo.data = {};
-    wndInfo.data.time = buffer.readUInt32BE(offset);
+    wndInfo.data.time = time;
     wndInfo.data.process = process.data;
     wndInfo.data.title = title.data;
     wndInfo.offset = title.offset;
